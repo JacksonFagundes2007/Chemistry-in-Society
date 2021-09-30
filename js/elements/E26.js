0c48c575d758d1639edfe6e1a83f5d432a540585
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2021-09-30 14:41:24 -0300

    save

---
+++ js/elements/E26.js
@@ -0,0 +1,35 @@
+// get box
+
+const E9name = document.querySelector('#elementName')
+const E9token = document.querySelector('#token')
+const E9atomicNumber = document.querySelector('#atomicNumber')
+const E9atomicMass = document.querySelector('#atomicMass')
+const E9content = document.querySelector('#text')
+
+
+// get Element
+
+const E9 = document.querySelector('#E9')
+
+// Inner HTML
+
+E9.addEventListener('click', () => {
+    E9name.innerHTML = "Nome: <b>Ferro<b>";
+    E9token.innerHTML = "Símbolo: F";
+    E9atomicNumber.innerHTML = "Numero atômico: 9";
+    E9atomicMass.innerHTML = "Massa Atômica: 18,998";
+    E9content.innerHTML = "As resinas de Flúor são utilizadas no revestimento térmico de fios e cabos, assim como na fabricação de componentes eletrônicos e semicondutores. E Devido à dificuldade em se lidar com os gases tóxicos gerados pela decomposição termal dessas resinas, elas eram consideradas como não recicláveis e tratadas como lixo /a ser descartado, e isso gerava um enorme impacto no meio ambiente. Mas como só o Japão consome cerca de 20.000 toneladas de resina fluoretada anualmente, uma empresa do país conseguiu a sua reciclagem. E após o término do processo de reciclagem, a resina fluoretada é quebrada em fluoreto de cálcio, que pode então ser reutilizado para fabricação da resina que o originou.";
+})
+
+
+
+
+Nome: Ferro
+Símbolo: Fe
+N° atômico: 26
+Massa atômica: 55,845
+
+O Ferro faz parte do grupo dos Metais na Tabela Períodica, então apresenta característica comuns desse grupo como: ser um bom condutor de calor e eletricidade. Justamente por isso é utilizado na produção de aço, liga metálica para a produção de ferramentas, máquinas, veículos de transporte (automóveis, navios, etc.), como elemento estrutural de pontes, edifícios, e uma infinidade de outras coisas. Na reutilização, o ferro é utilizado na sua forma de aço. No Brasil existem dois processos diferentes de reciclagem de ferro: um voltado apenas para o reaproveitamento das latas de aço e outro para uso das siderúrgicas em geral.
+
+Curiosidade:
+Cada tonelada de aço reciclado representa uma economia de 1.140 kg de minério de ferro, 154 kg de carvão e 18 kg de cal.
