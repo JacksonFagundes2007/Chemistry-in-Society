a41fce8787e1018fe8a3ac4ccb9e4d84f34ac70b
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2021-10-01 12:42:06 -0300

    save

--- js/elements/E9.js
+++ js/elements/E9.js
@@ -17,9 +17,9 @@ const E9 = document.querySelector('#E9')
 E9.addEventListener('click', () => {
 
     E9image.setAttribute('src', "https://media.discordapp.net/attachments/801974522839826484/893465632161534012/009.png")
-    E9name.innerHTML = "Nome: Ferro";
-    E9token.innerHTML = "Símbolo: Fe";
-    E9atomicNumber.innerHTML = "Numero atômico: 9";
-    E9atomicMass.innerHTML = "Massa Atômica: 55,845";
-    E9content.innerHTML = "O Ferro faz parte do grupo dos Metais na Tabela Períodica, então apresenta característica comuns desse grupo como: ser um bom condutor de calor e eletricidade. Justamente por isso é utilizado na produção de aço, liga metálica para a produção de ferramentas, máquinas, veículos de transporte (automóveis, navios, etc.), como elemento estrutural de pontes, edifícios, e uma infinidade de outras coisas. Na reutilização, o ferro é utilizado na sua forma de aço. No Brasil existem dois processos diferentes de reciclagem de ferro: um voltado apenas para o reaproveitamento das latas de aço e outro para uso das siderúrgicas em geral.";
+    E9name.innerHTML = "Nome: Flúor";
+	E9token.innerHTML = "Símbolo: F"; 
+	E9atomicNumber.innerHTML = "Numero atômico: 9"; 
+	E9atomicMass.innerHTML = "Massa Atômica: 18,998"; 
+    	E9content.innerHTML = " As resinas de Flúor são utilizadas no revestimento térmico de fios e cabos, assim como na fabricação de componentes eletrônicos e semicondutores. E         Devido à dificuldade em se lidar com os gases tóxicos gerados pela decomposição termal dessas resinas, elas eram consideradas como não recicláveis e tratadas como lixo a           ser descartado, e isso gerava um enorme impacto no meio ambiente. Mas como só o Japão consome cerca de 20.000 toneladas de resina fluoretada anualmente, uma empresa do             país conseguiu a sua reciclagem. E após o término do processo de reciclagem, a resina fluoretada é quebrada em fluoreto de cálcio, que pode então ser reutilizado para              fabricação da resina que o originou.";
 })
