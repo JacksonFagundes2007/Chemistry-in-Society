66d2badc1d5b7ece7963d9f51ef7df679a1af1db
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2021-10-02 13:42:25 -0300

    responsive

--- js/responsive.js
+++ js/responsive.js
@@ -1,7 +1,21 @@
+// page
+const body = document.querySelector('body')
+const content = document.querySelector('#content')
+
+// table
+const table = document.querySelector('#table')
+
+// box
+const box = document.querySelector('#box')
+
 window.addEventListener('load', () => {
     let width = window.innerWidth
 
     if (width < 1450) {
-        document.body.style.zoom = 0.9
+        content.style.flexDirection = "column"
+        box.style.marginLeft = "0"
+        box.style.width = "100vw"
+        body.style.overflowY = "scroll"
+        table.style.marginBottom = "100px"
     }
 })
