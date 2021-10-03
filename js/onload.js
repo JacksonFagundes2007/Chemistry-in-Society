92f464408026a2be0a0d849b49d0b179b570419f
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2021-10-03 17:02:46 -0300

    loading img

--- js/onload.js
+++ js/onload.js
@@ -1,3 +1,5 @@
-document.addEventListener('onload', () => {
-    
+const allImgs = document.querySelector('.line>img')
+
+allImgs.document.addEventListener('onload', () => {
+    allImgs.setAttribute('src', 'https://cdn.discordapp.com/attachments/886946995812577320/894313152102170674/loading-buffering.gif')
 })
